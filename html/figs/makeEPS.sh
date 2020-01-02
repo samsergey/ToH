f7b297e866a3446ff5a595b26956fe703e5bee8f
Sergey at ssb <samsergey@yandex.ru>
2020-01-02 22:56:46 +1200

    autocroppimg EPSes!

--- html/figs/makeEPS.sh
+++ html/figs/makeEPS.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 #
-# pdfsplit [input.pdf] [first_page] [last_page] [output.pdf] 
-#
-# Example: pdfsplit big_file.pdf 10 20 pages_ten_to_twenty.pdf
-#
-# written by: Westley Weimer, Wed Mar 19 17:58:09 EDT 2008
-#
-# The trick: ghostscript (gs) will do PDF splitting for you, it's just not
-# obvious and the required defines are not listed in the manual page. 
+# input -- pdf file-name 
 
 gs -q -dNOCACHE -dNOPAUSE -dBATCH -dSAFER -sDEVICE=eps2write -sOutputFile=$1-%d.eps $1.pdf
+
+# cropping EPS
+files=$1-*.eps
+for f in $files
+do
+    ps2epsi $f tmp.eps
+    mv tmp.eps $f
+done

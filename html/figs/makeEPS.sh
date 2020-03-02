0194b0892222a2c5fefff4e691a7bebd5d866712
Sergey at ssb <samsergey@yandex.ru>
2020-03-03 09:52:46 +1200

    update

--- html/figs/makeEPS.sh
+++ html/figs/makeEPS.sh
@@ -5,9 +5,9 @@
 gs -q -dNOCACHE -dNOPAUSE -dBATCH -dSAFER -sDEVICE=eps2write -dLanguageLevel=3 -sOutputFile=$1-%d.eps $1.pdf
 
 # cropping EPS
-files=$1-*.eps
+files=$1-*.pdf
 for f in $files
 do
-    ps2epsi $f tmp.eps
+    inkscape $f --export-eps $f.eps
     mv tmp.eps $f
 done
